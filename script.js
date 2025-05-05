afd371c87576d653b777b5e7e34bffb05696c5b5
“Richman” <205178813+richie-sol@users.noreply.github.com>
2025-05-05 22:44:10 +0100

    added a function

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const messageEl = document.getElementById("message")
 
 const resetEl = document.getElementById("reset")
 
+function displayMessage(message){
+    return resultEl.innerHTML = message
+}
 
 buttonEl.addEventListener("click", function(){
         let weight = weightEl.value
@@ -20,18 +23,18 @@ buttonEl.addEventListener("click", function(){
             const bmi = Number(weight) / (Number(height) * Number(height))
             
             if(bmi < 18.5){
-                resultEl.innerHTML = `<p>Your BMI is: <b style="color:#12A0DC">${bmi.toFixed(1)}</b></p>`
+                displayMessage(`<p>Your BMI is: <b style="color:#12A0DC">${bmi.toFixed(1)}</b></p>`)
                 messageEl.innerHTML = `<p>You are <b style="color:#12A0DC">underweight</b>😢</p>`
             }else if(bmi >= 18.5 && bmi < 25){
-                resultEl.innerHTML = `<p>Your BMI is: <b style="color:#11BD1F">${bmi.toFixed(1)}</b></p>`
+                
+                displayMessage(`<p>Your BMI is: <b style="color:#11BD1F">${bmi.toFixed(1)}</b></p>`)
                 messageEl.innerHTML = `<p>You have a <b style="color:#11BD1F">healthy weight</b>, that's great 🙌🏽</p>`
             }else if(bmi >= 25 && bmi < 30){
-                resultEl.innerHTML = `<p>Your BMI is: <b style="color:#901E1E">${bmi.toFixed(1)}</b></p>`
+                displayMessage(`<p>Your BMI is: <b style="color:#901E1E">${bmi.toFixed(1)}</b></p>`)
                 messageEl.innerHTML = `<p>You are <b style="color:#901E1E">overweight</b>😢</p>`
             }else if(bmi >= 30){
-                resultEl.innerHTML = `<p>Your BMI is: <b style="color:#BD2626">${bmi.toFixed(1)}</b></p>`
+                displayMessage(`<p>Your BMI is: <b style="color:#BD2626">${bmi.toFixed(1)}</b></p>`)
                 messageEl.innerHTML = `<p>You are <b style="color:#BD2626">obese</b>, you need to hit the gym🏋🏽‍♂️ 😔</p>`
-                // document.querySelector("b").style.color = "green"
                 
             }
             
